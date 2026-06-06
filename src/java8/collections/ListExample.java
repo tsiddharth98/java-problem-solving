caa6d7f62be733c0fc932620dc78aa6bfdd89f41
Siddharth <tsiddharth98@gmail.com>
2026-06-06 07:05:48 +0530

    Renamed List class to ListExample

--- src/java8/collections/ListExample.java
+++ src/java8/collections/ListExample.java
@@ -2,7 +2,7 @@ package java8.collections;
 
 import java.util.HashMap;
 
-public class List {
+public class ListExample {
 
     HashMap<Object, Object> map = new HashMap<>();
 }
--- src/patterns/Pattern02.java
+++ src/patterns/Pattern02.java
@@ -3,6 +3,6 @@ package patterns;
 public class Pattern02 {
 
     public static void main(String[] args) {
-        
+
     }
 }
