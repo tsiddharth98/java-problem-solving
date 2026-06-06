92862dccb2653bde1f3423c1ddc7826f11fe595f
Siddharth <tsiddharth98@gmail.com>
2026-06-06 21:55:33 +0530

    pattern15 completed

--- src/patterns/Pattern15.java
+++ src/patterns/Pattern15.java
@@ -17,22 +17,13 @@ public class Pattern15 {
 
     public static void main(String[] args) {
         method1();
-        method2();
     }
 
     private static void method1() {
-        for(int row=1;row<=5;row++) {
-            for(int col=1;col<=row;col++) {
-                System.out.print((5-col+1)+" ");
-            }
-            System.out.println();
-        }
-    }
-
-    private static void method2() {
-        for(int row=1;row<=5;row++) {
-            for(int col=1;col<=5-(row-1);col++) {
-                System.out.print(col+" ");
+        for(int row=5;row>=1;row--) {
+            for(int col=1;col<=5;col++) {
+                if(col>=row) System.out.print("* ");
+                else System.out.print("  ");
             }
             System.out.println();
         }
